64c800f2b5a110c5f94beb5ff5c6eb227ff95c66
s030816 <zenon382@gmail.com>
2023-03-13 22:44:41 +0200

    more naming

--- CardTest/Pages/Privacy.cshtml.cs
+++ CardTest/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CardTest.Pages
+namespace CardGame.Pages
 {
     public class PrivacyModel : PageModel
     {
