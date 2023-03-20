b7d7db04c821d5c5ce81da40395a1893b495fe65
s030816 <s030816@stud.viko.lt>
2023-03-20 20:05:34 +0200

    Azure AdAuth

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -1,23 +1,62 @@
 using MudBlazor.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+builder.Services.AddRazorPages()
+    .AddMicrosoftIdentityUI();
+
+builder.Services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    options.Events = new OpenIdConnectEvents
+    {
+        OnRedirectToIdentityProvider = (context) =>
+        {
+            // Override the redirect_uri
+            //  Ideally extract this from config 
+            //  Or context.Request.Headers["X-Forwarded-Host"]
+            //  see: https://learn.microsoft.com/en-us/azure/frontdoor/front-door-http-headers-protocol#front-door-to-backend
+
+            context.ProtocolMessage.RedirectUri
+                = "https://lcepa381.uk/signin-oidc";
+            return Task.FromResult(0);
+        }
+    };
+});
+
+// Add services to the container.
 builder.Services.AddMudServices();
 
 builder.Services.AddServerSideBlazor(o => o.DetailedErrors = true);
 
 var app = builder.Build();
 
-
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedProto
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -25,6 +64,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 //app.AddComponent<Testas>("testas");
